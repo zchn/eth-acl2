e885c94b811b2191e86c8977bf324ec3c55fa43f
Kevin Chen <kevin.zchn@gmail.com>
2018-03-18 16:12:35 -0400

    Disable rule-classes for stack-defthms.lisp

--- stack-defthms.lisp
+++ stack-defthms.lisp
@@ -4,12 +4,15 @@
 
 (defthm stack/push-io-validp
     (implies (stack/validp s)
-             (stack/validp (stack/push s v))))
+             (stack/validp (stack/push s v)))
+  :rule-classes nil)
 
 (defthm stack/pop-io-validp
     (implies (stack/validp s)
-             (stack/validp (stack/pop s))))
+             (stack/validp (stack/pop s)))
+  :rule-classes nil)
 
 (defthm stack/popn-io-validp
     (implies (stack/validp s)
-             (stack/validp (stack/popn s n))))
+             (stack/validp (stack/popn s n)))
+  :rule-classes nil)
