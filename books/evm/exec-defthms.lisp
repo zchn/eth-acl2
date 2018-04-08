ce25e74b498aa95c1ea758fdb62a771d0eeed351
Kevin Chen <kevin.zchn@gmail.com>
2018-04-08 17:34:52 -0400

    clean up experiments

--- books/evm/exec-defthms.lisp
+++ books/evm/exec-defthms.lisp
@@ -24,17 +24,3 @@
              (env/validp (env/exec e)))
   :hints (("Goal"
            :in-theory (disable env/validp env/exec-hacky))))
-
-;; ---- Just some experiments ----
-
-;; (defthm env/step-io-pc-validp
-;;     (implies (env/validp e)
-;;              (integerp (env/pc (env/step e))))
-;;   :hints (("Goal" :in-theory (enable env/step))))
-
-
-;; The proof checking takes 12000+ seconds.
-;; (defthm env/exec-hacky-io-pc-validp
-;;     (implies (env/validp e)
-;;              (integerp (env/pc (env/exec-hacky e n))))
-;;   :hints (("Goal" :in-theory (disable env/step))))
