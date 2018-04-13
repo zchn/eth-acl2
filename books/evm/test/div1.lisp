ca808d2c27ea6b97a0d50130888590892c8c689c
Kevin Chen <kevin.zchn@gmail.com>
2018-04-12 20:37:19 -0400

    Add div1

---
+++ books/evm/test/div1.lisp
@@ -0,0 +1,75 @@
+
+(in-package "ACL2")
+
+(include-book "../env")
+(include-book "../exec")
+
+(defun mk-initial-env-div1 ()
+
+  (mk-env
+    ;; pc
+    0
+    ;; rom
+    (mk-rom "60027ffedcba9876543210fedcba9876543210fedcba9876543210fedcba98765432100460005260206000f3")
+    ;; stack
+    (mk-empty-stack)
+    ;; mem
+    (mk-empty-memory)
+    ;; storage
+    (mk-empty-storage)
+    ;; halted
+    nil
+    ;; gas
+    100000
+    ;; context
+  
+    (mk-context
+      ;; ia
+      87579061662017136990230301793909925042452127430
+      ;; io
+      1170859069521887415590932569929099639409724315265
+      ;; ip
+      100000000000000
+      ;; id
+      "0x"
+      ;; is
+      1170859069521887415590932569929099639409724315265
+      ;; iv
+      1000000000000000000
+      ;; ih
+  
+      (mk-block-header
+        ;; ihc
+        244687034288125203496486448490407391986876152250
+        ;; ihs
+        1
+        ;; ihi
+        0
+        ;; ihd
+        256
+        ;; ihl
+        1000000)
+      ;; ie
+       "<call/create stack height>"
+      ;; iw
+      "<permission>")
+    ;; substate
+  
+    (mk-substate
+      ;; suiside
+      nil
+      ;; log
+      nil
+      ;; touched
+      nil
+      ;; refund
+      0)))
+
+(defun env-with-pre-div1 ()
+    (mk-initial-env-div1))
+
+(defun env-with-post-div1 ()
+    (mk-initial-env-div1))
+(defthm storage-equiv-div1
+  (alist-equiv (env/storage (env/exec (env-with-pre-div1)))
+               (env/storage (env-with-post-div1))))
