9e4f2db63d5a2587365cfe02d83a750846a2bd8a
Kevin Chen <kevin.zchn@gmail.com>
2018-07-05 10:41:59 -0400

    Initial settings for pyacl2.

---
+++ pyacl2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyacl2',
+      version='0.1',
+      description='Python bindings for ACL2',
+      url='http://github.com/zchn/eth-acl2',
+      author='Kevin Chen',
+      author_email='k_@berkeley.edu',
+      license='Apache',
+      packages=['pyacl2'])
