b8c00af0735b6a2a183a59f20988857aaf247c40
Kevin Chen <kevin.zchn@gmail.com>
2018-07-29 19:12:55 -0400

    Add additional cookiecutter templates.

--- pyacl2/pyacl2/__init__.py
+++ pyacl2/pyacl2/__init__.py
@@ -1,1 +1,9 @@
-from .bridge import *
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PyAcl2."""
+
+__author__ = """Kevin Chen"""
+__email__ = 'k_@berkeley.edu'
+__version__ = '0.1.0'
+
+from pyacl2.bridge import *
--- pyacl2/setup.py
+++ pyacl2/setup.py
@@ -1,10 +1,55 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-setup(name='pyacl2',
-      version='0.1',
-      description='Python bindings for ACL2',
-      url='http://github.com/zchn/eth-acl2',
-      author='Kevin Chen',
-      author_email='k_@berkeley.edu',
-      license='Apache',
-      packages=['pyacl2'])
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Kevin Chen",
+    author_email='k_@berkeley.edu',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Python bindings for ACL2 Bridge.",
+    entry_points={
+        'console_scripts': [
+            'pyacl2=pyacl2.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='pyacl2',
+    name='pyacl2',
+    packages=find_packages(include=['pyacl2']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/zchn/pyacl2',
+    version='0.1.0',
+    zip_safe=False,
+)
